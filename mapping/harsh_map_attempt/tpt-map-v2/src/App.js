d5d4fe086d906c6b346b062dcb69adeb155ccf14
Dea Bardhoshi <dea.bardhoshi@berkeley.edu>
2023-08-10 18:09:47 -0400

    minor fixes to map

--- mapping/harsh_map_attempt/tpt-map-v2/src/App.js
+++ mapping/harsh_map_attempt/tpt-map-v2/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import MapGL, { Marker, Popup } from 'react-map-gl';
 import data from './master_data.json';
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiaGFyc2gxNyIsImEiOiJjbGt1b2lrZW4wNTBmM2twaXJreHhjOTIxIn0.A_Cl-PJeK5lr6rDg8bY7lw'; // Replace with your Mapbox token
 
@@ -11,9 +12,25 @@ function App() {
     zoom: 10,
   });
 
-  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [PopupOpen, setPopupOpen] = useState(null);
 
-  console.log(data);
+  const mapdata = data.map((location) => (
+        
+    <Marker key={location} 
+              latitude={Number(location.lat)} 
+              longitude={Number(location.lng)} 
+              onClick={(e) => {
+                  e.originalEvent.stopPropagation();
+                  setPopupOpen(location) }
+                
+    }>  
+      <button>{location.name}</button> {/* Change this part */}
+
+  </Marker>
+
+  
+  ))
+
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
@@ -23,44 +40,34 @@ function App() {
         height="100%"
         mapStyle="mapbox://styles/mapbox/streets-v11"
         mapboxAccessToken={MAPBOX_TOKEN}
-        onViewportChange={(viewport) => setViewport(viewport)}
-      >
-        {data.map((location, index) => (
-          <Marker key={index} latitude={Number(location.lat)} longitude={Number(location.lng)}>
-          <div
-            onClick={(e) => {
-              e.preventDefault();
-              setSelectedLocation(location);
-            }}
-          >
-            <button>{location.name}</button> {/* Change this part */}
-          </div>
-        </Marker>
-        ))}
+  
+        onMove={evt => setViewport(evt.viewport)}
+    >
+      {mapdata}
 
-        {selectedLocation ? (
-          <Popup
-            latitude={selectedLocation.lat}
-            longitude={selectedLocation.lng}
-            onClose={() => setSelectedLocation(null)}
+        {PopupOpen && (
+          <Popup key={PopupOpen}
+            latitude={Number(PopupOpen.lat)}
+            longitude={Number(PopupOpen.lng)}
+            onClose={() => setPopupOpen(null)}
           >
             <div>
-              <h3>Name: {selectedLocation.name}</h3>
-              <p>Type: {selectedLocation.type}</p>
-              <p>City, State: {selectedLocation.city}, {selectedLocation.state}</p>
-              {selectedLocation.creators_rec && <p>Creator's Rec: Yes</p>}
-              <p>Notes: {selectedLocation.notes}</p>
+            <h3>Name: {PopupOpen.name}</h3>
+              <p>Type: {PopupOpen.type}</p>
+              <p>City, State: {PopupOpen.city}, {PopupOpen.state}</p>
+              {PopupOpen.creators_rec && <p>Creator's Rec: Yes</p>}
+              <p>Notes: {PopupOpen.notes}</p>
               <p>
-                G-Maps Rating: {selectedLocation.rating} (
-                {selectedLocation.user_ratings_total} ratings)
+                G-Maps Rating: {PopupOpen.rating} (
+                {PopupOpen.user_ratings_total} ratings)
               </p>
               <p>
                 Google Maps:{' '}
-                <a href={selectedLocation.google_maps_link}>Link</a>
+                <a href={PopupOpen.google_maps_link}>Link</a>
               </p>
             </div>
-          </Popup>
-        ) : null}
+          </Popup> )}
+
       </MapGL>
     </div>
   );
