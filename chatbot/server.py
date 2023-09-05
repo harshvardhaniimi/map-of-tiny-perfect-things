c189c4188f57c12f523b1d4d8b4f135465ae0eb4
Harshvardhan <40472851+harshvardhaniimi@users.noreply.github.com>
2023-09-05 19:48:30 -0400

    Prepare for Heroku deployment

--- chatbot/server.py
+++ chatbot/server.py
@@ -8,12 +8,23 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 import os
 import pandas as pd
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 def read_key_from_file(file_path):
     with open(file_path, 'r') as f:
         return f.read()
 
-os.environ["OPENAI_API_KEY"] = read_key_from_file("../../openai_key.txt")
+os.environ["OPENAI_API_KEY"] = read_key_from_file("../openai_key.txt")
 
 prompt_message = """
 For this task, you are a helpful chatbot named Ava, Wall-E's friend. You are designed to be helpful in finding interesting places to eat, have a coffee, and hangout. Specifically, you are a chatbot who retrieves information from the corresponding database and respond to the user about the places you have.
@@ -31,7 +42,7 @@ How can I help you?"
 
 def get_cities_and_countries():
     # Read the master_data CSV file
-    file_path = "../../master_data/master_data.csv"
+    file_path = "../master_data/master_data.csv"
     df = pd.read_csv(file_path)
 
     # Extract unique city, state, and country combinations
@@ -47,44 +58,48 @@ city_names = get_cities_and_countries()
 initial_message = prompt_message.replace("{name of cities with country}", city_names)
 
 # Loading the data
-loader = CSVLoader("../../master_data/master_data.csv")
+loader = CSVLoader("../master_data/master_data.csv")
 places_docs = loader.load_and_split() #default is 1000 tokens
 
 # initailise an embeddings model
 embeddings = OpenAIEmbeddings()
 txt_docsearch = Chroma.from_documents(places_docs, embeddings, persist_directory="places_persist")
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.1)
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.8)
 qa = ConversationalRetrievalChain.from_llm(llm, retriever=txt_docsearch.as_retriever())
 
-async def handle_client(websocket, path):
-    # Create an individual chat history for this client
-    chat_history = []
-    
+chat_histories = {}
+
+@socketio.on('connect')
+def handle_connect():
+    # Create an individual chat history for each client
+    chat_histories[request.sid] = []
+
     # Initialize a new chat session with the initial prompt
-    response = qa({"question": initial_message, "chat_history": chat_history})
-    chat_history.append((initial_message, response["answer"]))
+    response = qa({"question": initial_message, "chat_history": chat_histories[request.sid]})
+    chat_histories[request.sid].append((initial_message, response["answer"]))
 
-    # send the first response
-    await websocket.send(response["answer"])
+    # Send the first response
+    emit('reply', response["answer"])
 
-    async for message in websocket:
+@socketio.on('message')
+def handle_message(message):
+    try:
+        chat_history = chat_histories[request.sid]
         
         # Respond to predefined commands
         if message == "ping":
-            await websocket.send("pong")
+            answer = "pong"
         elif message == "I am Pablo":
-            await websocket.send("Really? You are so cool you are like the coolest person ever born and humble too. And handsome")
+            answer = "Really? You are so cool you are like the coolest person ever born and humble too. And handsome"
         else:
-            try:
-                question = message
-                response = qa({"question": question, "chat_history": chat_history})
-                answer = response["answer"]
-                chat_history.append((question, answer))
-                await websocket.send(answer)
-            except Exception as e:
-                await websocket.send("<b>Error:</b> " + str(e))
+            response = qa({"question": message, "chat_history": chat_history})
+            answer = response["answer"]
+            chat_history.append((message, answer))
+        
+        emit('reply', answer)
+    except Exception as e:
+        emit('reply', "<b>Error:</b> " + str(e))
 
-start_server = websockets.serve(handle_client, "localhost", 8765)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+if __name__ == "__main__":
+    socketio.run(app)
